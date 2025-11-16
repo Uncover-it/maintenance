29f954699a54844529d0b5a729b21f097bd3a3ab
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-11-16 21:12:51 +0530

    optimize this 🙏

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,17 @@ import Spinner from "@/components/ui/spinner";
 import { Wrench } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-6 row-start-2 sm:items-start">
-        <Spinner size={128} className=" w-full" />
+    <div className="font-sans grid place-items-center min-h-dvh px-8 gap-16">
+      <main className="flex flex-col gap-6">
+        <Spinner size={128} />
         <h1 className="font-mono text-4xl text-center">
           We&apos;ll be right back
         </h1>
         <Link
-          className={cn(buttonVariants({ size: "lg" }), "w-full")}
+          className={buttonVariants({ size: "lg" })}
           href={"https://status.uncoverit.org"}
           target="_blank"
         >
